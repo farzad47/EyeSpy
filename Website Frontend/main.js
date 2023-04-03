4f595d2d221165c4ebbed00cbdeab2e724cb0a1d
verma53665 <125225991+verma53665@users.noreply.github.com>
2023-04-02 20:00:45 -0400

    website UI changes
    full UI with login and dashboard page

--- Website Frontend/main.js
+++ Website Frontend/main.js
@@ -34,11 +34,47 @@ function clickEvents(){
                 // if you need a callback function
             });
         }
+        else if($(this).hasClass('Demo')){
+            $('html, body').animate({
+                scrollTop: $('body').offset().top = 950
+            }, 800, function() {
+                // if you need a callback function
+            });
+        }
     });
-  
+    $(".loginBtnSubmit").on("click", function(e){
+        if($('input[name="loginEmail"]').val() != '' & $('input[name="loginPassword"]').val() != ''){
+            location.href = "http://127.0.0.1:5500/Website%20Frontend/login.html";
+        }
+        
+    })
 
     $( ".getDemoBtn" ).on( "click", function() {
         console.log( "click" );
     });
+
+    $('.getDemoBtn').on("click", function(){
+        $(".form-popup")[0].style.display = "block";
+    })
+    $('.getAppointBtn').on("click",function(){
+        $(".form-popup")[0].style.display = "block";
+    })
+    $('.btn-login').on("click",function(){
+        $("#loginForm")[0].style.display = "block";
+    })
+    $('.getDemoBtn').on("click",function(){
+        $("#myForm")[0].style.display = "block";
+    })
+    $('.btn-logout').on("click",function(){
+        location.href = "index.html";
+    })
+    $('.cancel').on('click',function(){
+        $(".form-popup")[0].style.display = "none";
+    })
+    function onSignIn(googleUser) {
+        var id_token = googleUser.getAuthResponse().id_token;
+        // Send the ID token to your server to authenticate the user
+      }
+
     
 }

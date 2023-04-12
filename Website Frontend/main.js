b2882f729a432b15ab2b2a0309420628e7afb93f
verma53665 <125225991+verma53665@users.noreply.github.com>
2023-04-11 23:10:07 -0400

    upload to file.io

--- Website Frontend/main.js
+++ Website Frontend/main.js
@@ -8,7 +8,10 @@ $(document).ready(function () {
     if(localStorage.getItem("customer_id") != null){
         getHistoryData();
     }
+    
 })
+
+var imgArr = [];
 function clickEvents() {
     $(".lowHead ul li").on("click", function (e) {
         if (!$(this).hasClass('pageSelected')) {
@@ -82,24 +85,13 @@ function clickEvents() {
         var nameTemp = $("input[name='name']").val()
        var email= $("input[name='email']").val()
   var phone= $("input[name='phone']").val()
-  var photos= "configAWS"
+  var photos= imgArr.toString()
   var carrier= $("select[name='carrier']").val()
   var address = $("input[name='Address']").val()
   $(".error").addClass('hide')
-        submitDemoDetails(nameTemp,email,phone,photos,carrier,address)
+
+     submitDemoDetails(nameTemp,email,phone,photos,carrier,address)
     })
-
-    function goPython(){
-        uploadtoS3().then(function(resp){
-            console.log(resp)
-            // window.location.href="http://127.0.0.1:5000/"
-        });
-       
-    }
-
-    
-
-    // console.log(uploadedImage.Location)
 }
 
 
@@ -170,7 +162,6 @@ function getHistoryData(){
         dataType: 'json',
         success: function(data){
                 const table = document.getElementById("historyTable");
-               
                 headings = ["Sr No.", "Name", "Alert Sent","Date and Time","Access Type"]
                 for(var i=0; i < data.length ; i++){
                     let row = table.insertRow();
@@ -178,8 +169,7 @@ function getHistoryData(){
                        var temp =  row.insertCell(j);
                        temp.innerHTML = data[i][j];
                     }
-                }
-              
+                }    
         },
         error: function(xhr){
             app.log("Device control failed");
@@ -191,12 +181,13 @@ function getHistoryData(){
 }
 
 function submitDemoDetails(name,email,phone,photo,carrier,address){
+    
     $.ajax({
         contentType: 'application/json',
         data: JSON.stringify({"user_name":name,"email_id": email ,"phone_no":phone,"photos_link":photo,"carrier":carrier,"address":address}),
         dataType: 'json',
         success: function(data){
-            window.location.href = "login.html";
+            window.location.href = "http://localhost:7777";
            
         },
         error: function(xhr){
@@ -232,30 +223,141 @@ function submitDemoDetails(name,email,phone,photo,carrier,address){
 }
 
 
+function uploadtoS3(photofile){
 
-var s3 = new AWS.S3({
-    accessKeyId: 'AKIAYYI7C6SJKZZZKZUO',
-    secretAccessKey: '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'
-  });
-  
-  var uploadPreSignedUrl = s3.getSignedUrl('putObject', {
-      Bucket: 'dcproject123456',
-      Key: '482905.jpg',
-      ACL: 'authenticated-read',
-      // This must match with your ajax contentType parameter
-      ContentType: 'binary/octet-stream'
-  
-      /* then add all the rest of your parameters to AWS puttObect here */
-  });
-  
-  var downloadPreSignedUrl = s3.getSignedUrl('getObject', {
-      Bucket: 'dcproject123456',
-      Key: '482905.jpg',
-      /* set a fixed type, or calculate your mime type from the file extension */
-      ResponseContentType: 'image/jpeg'
-      /* and all the rest of your parameters to AWS getObect here */
-  });
-  
-  // now you have both urls
-  console.log( uploadPreSignedUrl, downloadPreSignedUrl ); 
+    var bucketName = "dcproject123456";
+    var region = "";
+    var accessKeyId = 'AKIAYYI7C6SJKZZZKZUO';
+    var secretAccessKey = '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';
+
+    var s3 = new AWS.S3({
+        region,
+        accessKeyId,
+        secretAccessKey, 
+      });
+
+      const fileStream = fs.createReadStream(photofile);
+
+      var uploadParams ={
+        Bucket: bucketName,
+        Body:fileStream,
+        Key: "testing"
+      }
+      debugger;
+      var output = s3.upload(uploadParams) 
+      debugger;
+    //   var uploadPreSignedUrl = s3.getSignedUrl('putObject', {
+    //       Bucket: 'dcproject123456',
+    //       Key: '482905.jpg',
+    //       ACL: 'authenticated-read',
+    //       // This must match with your ajax contentType parameter
+    //       ContentType: 'binary/octet-stream'
+      
+    //       /* then add all the rest of your parameters to AWS puttObect here */
+    //   });
+      
+    //   var downloadPreSignedUrl = s3.getSignedUrl('getObject', {
+    //       Bucket: 'dcproject123456',
+    //       Key: '482905.jpg',
+    //       /* set a fixed type, or calculate your mime type from the file extension */
+    //       ResponseContentType: 'image/jpeg'
+    //       /* and all the rest of your parameters to AWS getObect here */
+    //   });
+      
+    //   // now you have both urls
+    //   console.log( uploadPreSignedUrl, downloadPreSignedUrl );
+
+    //   var theFormFile = photofile;
+     
+
+      
+}
+
+
+function s3upload(photofile) {
+
+    var bucketName = "dcproject123456";
+    var bucketRegion = "us-east-2";
+    var accessKeyId = 'AKIAYYI7C6SJKZZZKZUO';
+    var secretAccessKey = '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';
+
+    var files = photofile;
+    if (files) 
+    {
+      var file = files[0];
+      var fileName = file.name;
+      var filePath = 'dcproject123456/' + fileName;
+      var fileUrl = 'https://' + bucketRegion + '.amazonaws.com/dcproject123456/' +  filePath;
+      AWS.S3.upload({
+         Key: fileUrl,
+         Body: file,
+         ACL: 'public-read'
+         }, function(err, data) {
+         if(err) {
+         reject('error');
+         }
+         alert('Successfully Uploaded!');
+         }).on('httpUploadProgress', function (progress) {
+         var uploaded = parseInt((progress.loaded * 100) / progress.total);
+         $("progress").attr('value', uploaded);
+       });
+    }
+ };
+
+
+  function uploadtoCloud(photo){
+
+    
+
+    // const formData = new FormData()
+    // var stri = (Math.random() + 1).toString(36).substring(7)
+    // formData.append('public_id',stri) //name of photo on cloud
+    // // formData.append('api_key', '217928831636613') 
+    // formData.append('upload_preset', 'sih0nbjx') //name of photo on cloud
+    // formData.append('file', photo,'test.jpg') //name of photo on cloud
+
+    // $.ajax({
+    //     formData: formData,
+    //     processData: false,
+    //     contentType: false,
+    //     success: function(data){
+    //             debugger;    
+    //     },
+    //     error: function(xhr){
+    //         debugger;
+    //     },
+    //     processData: false,
+    //     type: 'POST',
+    //     url: 'https://api.cloudinary.com/v1_1/dr6uwp8cq/image/upload'
+    // })
+ }
  
+
+ const fileInput = document.querySelector("#fileInput");
+
+const uploadFile = file => {
+  console.log("Uploading file...");
+  const API_ENDPOINT = "https://file.io";
+  const request = new XMLHttpRequest();
+  const formData = new FormData();
+
+  request.open("POST", API_ENDPOINT, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status == 200) {
+        debugger;
+        imgArr.push(JSON.parse(request.responseText).link)
+        console.log(request.responseText);
+    }
+  };
+  formData.append("file", file);
+  request.send(formData);
+};
+
+fileInput.addEventListener("change", event => {
+  const files = event.target.files;
+  for(var i=0; i < files.length ; i++){
+    uploadFile(files[i]);
+  }
+ 
+  
+});
